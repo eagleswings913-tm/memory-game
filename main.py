ecdf816ef68c06c55629ea4cd1dbc0a9d0d137a3
Tricia Miller <eagleswings913@gmail.com>
2026-09-25 15:58:16 -0400

    Added basic logic for inputs, turning over cards, and testing for a match

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ game_grid = [[['', ''] for _ in range(cols)] for _ in range(rows)]
 
 icons = ['❤️', '❤️', '❄️', '❄️', '🔵', '🔵', '🟪','🟪', '⭐','⭐', '🔶', '🔶']
 
-def print_game_grid(game_grid):
+def print_game_grid(game_grid, card1, card2, match_flag):
     grid = '_____________________________\n|      |      |      |      |\n'
     for j in range(rows):
         for i in range(cols):
@@ -14,7 +14,20 @@ def print_game_grid(game_grid):
                 grid = grid + '|   '
             else:
                 grid = grid + '|  '
-            grid = grid + game_grid[j][i][0]
+            if card1 == 0:
+               grid = grid + game_grid[j][i][0]
+            else:
+                if card1 == game_grid[j][i][0] or card2 == game_grid[j][i][0]:
+                    if match_flag == 1:
+                        if game_grid[j][i][1] == "-":
+                            if int(game_grid[j][i][0]) > 9:
+                                grid = grid + '--'
+                            else:
+                                grid = grid + game_grid[j][i][1]
+                    else:
+                        grid = grid + game_grid[j][i][1]
+                else:
+                    grid = grid + game_grid[j][i][0]
             if i == len(game_grid[0]) - 1:
                 if int(game_grid[j][i][0]) <= 9:
                     grid = grid + '  |'
@@ -45,4 +58,36 @@ def initialize_grid(grid, pics):
 
 game_grid = initialize_grid(game_grid, icons)
 
-print_game_grid(game_grid)
+print_game_grid(game_grid, card1 = 0, card2 = 0, match_flag=0)
+print(game_grid)
+turns = 1
+choice_2 = ""
+choice_1 = input("Choose a square: \n")
+card_1 = int(choice_1) - 1
+card_1_row = card_1 // cols
+card_1_col = card_1 % cols
+square1_value = game_grid[card_1 // cols][card_1 % cols][0]
+square1_pic = game_grid[card_1 // cols][card_1 % cols][1]
+if square1_value == str(card_1 + 1):
+    print_game_grid(game_grid, choice_1, choice_2, 0)
+
+choice_2 = input("Choose a square: \n")
+card_2 = int(choice_2) - 1
+card_2_row = card_2 // cols
+card_2_col = card_2 % cols
+square2_value = game_grid[card_2 // cols][card_2 % cols][0]
+square2_pic = game_grid[card_2 // cols][card_2 % cols][1]
+if square2_value == str(card_2 + 1):
+    print_game_grid(game_grid, choice_1, choice_2, 0)
+
+if square1_pic == square2_pic:
+    print("The cards match!")
+    game_grid[card_1 // cols][card_1 % cols][1] = "-"
+    game_grid[card_2 // cols][card_2 % cols][1] = "-"
+    print(game_grid)
+    input("Press any key to continue...")
+    print_game_grid(game_grid, choice_1, choice_2, 1)
+else:
+    print("The cards do not match!")
+    input("Press any key to continue...")
+    print_game_grid(game_grid, card1 = 0, card2 = 0, match_flag=0)
